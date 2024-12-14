0f4fa0ac8caa6eaa220273eeb7415341aa603664
mahdiiii04 <143207634+mahdiiii04@users.noreply.github.com>
2024-12-15 00:43:31 +0100

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import yfinance as yf
-import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-import numpy as np
-import joblib
-import pickle
+import plotly.graph_objects as go
 
 @st.cache_data
 def get_data(start, end):
@@ -19,6 +19,7 @@ start_date = '2020-01-01'
 end_date = '2024-01-01'
 bitcoin_data = get_data(start_date, end_date)
 
+
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(bitcoin_data[['Adj Close']]) 
 
@@ -31,7 +32,6 @@ def create_sequences(data, time_step=60):
     return np.array(X), np.array(y)
 
 time_step = 60
-
 X, y = create_sequences(scaled_data, time_step)
 
 X = X.reshape(X.shape[0], X.shape[1], 1)
@@ -40,8 +40,15 @@ train_size = int(len(X) * 0.8)
 X_train, X_test = X[:train_size], X[train_size:]
 y_train, y_test = y[:train_size], y[train_size:]
 
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)),
+    tf.keras.layers.LSTM(units=50, return_sequences=False),
+    tf.keras.layers.Dense(units=1)
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(X_train, y_train, epochs=10, batch_size=32)
 
 predictions = model.predict(X_test)
 
